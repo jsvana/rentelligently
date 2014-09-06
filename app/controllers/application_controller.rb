84a42615867d487eeb5eadffd6818f7f5ded283a
Phil Middleton <pjmiddle@mtu.edu>
2014-09-05 23:23:50 -0400

    Made changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  layout :layout_by_resource
+
 	def layout_by_resource
-		if devise_controller? and user_signed_in?
+		if devise_controller? and not user_signed_in?
 			'login'
 		else
 			'application'
