e5a83a88ace4a31b12b315a5d905630f63ad7cb7
Jay Vana <jsvana@mtu.edu>
2014-09-07 01:31:42 -0400

    Todo!!!

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,5 +4,10 @@ class DashboardsController < ApplicationController
 	respond_to :html
 
 	def index
+		if current_user.has_current_property?
+			redirect_to current_user.current_property
+		else
+			redirect_to new_property_path
+		end
 	end
 end
